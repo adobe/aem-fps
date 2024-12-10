4f4bce381e1262e5f1fd5444eef574361eb357f8
Alexandre Capt <acapt@adobe.com>
2024-12-10 16:05:49 +0100

    feat: RUM v2 (#6)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -11,9 +11,4 @@
  */
 
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
-
-// Core Web Vitals RUM collection
-sampleRUM('cwv');
-
 // add more delayed functionality here
